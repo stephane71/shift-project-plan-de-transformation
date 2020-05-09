e25777887ad4f0b6e5b2df9618610bfe4715db72
stephane71 <stephanemaire71@gmail.com>
2020-05-09 12:52:14 +0200

    Cosmos decorator | import global css

--- cosmos.decorator.js
+++ cosmos.decorator.js
@@ -1,6 +1,8 @@
 import React from "react";
-import "./cosmos.styles.css";
 import CssBaseline from "@material-ui/core/CssBaseline";
+
+import "./cosmos.styles.css";
+import "./src/styles.css";
 
 export default ({ children }) => (
   <>
