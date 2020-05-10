206f6066a3a6979315af033447975fd888c2b73c
stephane71 <stephanemaire71@gmail.com>
2020-05-10 13:32:32 +0200

    NevMenu | provide ListItemMenu ListItemMenu | create component: wrap with  NextLink

--- src/components/NavMenu/index.js
+++ src/components/NavMenu/index.js
@@ -1,15 +1,45 @@
 import React from "react";
 import PropTypes from "prop-types";
+import NextLink from "next/link";
+
 import Divider from "@material-ui/core/Divider";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
 
 import ListBase from "../List";
 
+function ListItemMenu({ item }) {
+  const { icon, text } = item;
+
+  return (
+    <NextLink href={`/sector/${item.keyword}`} as={`/sector/${item.keyword}`}>
+      <ListItem button>
+        {icon && <ListItemIcon>{icon}</ListItemIcon>}
+        <ListItemText primary={text} />
+      </ListItem>
+    </NextLink>
+  );
+}
+
+ListItemMenu.propTypes = {
+  item: PropTypes.object.isRequired
+};
+
 function NavMenu({ items, onClickItem }) {
+  function renderItem(item) {
+    return <ListItemMenu key={item.id} item={item} />;
+  }
+
   return (
     <div>
       {items.map((item, i) => (
         <React.Fragment key={i}>
-          <ListBase items={item} onClickItem={onClickItem} />
+          <ListBase
+            items={item}
+            onClickItem={onClickItem}
+            renderItem={renderItem}
+          />
           <Divider />
         </React.Fragment>
       ))}
