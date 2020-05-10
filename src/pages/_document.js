36154f0486acf50ad8bb7b031a8b57642f860330
stephane71 <stephanemaire71@gmail.com>
2020-05-10 21:24:25 +0200

    Document | remove title

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -9,9 +9,6 @@ export default class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
-          <title>
-            The Shift Project - Plan de transformation de l'économie française
-          </title>
           <meta name="theme-color" content={theme.palette.primary.main} />
           <link
             rel="stylesheet"
