c58cbb32eec82fc510beebe4bb63aad60f470cfd
stephane71 <stephanemaire71@gmail.com>
2020-05-10 13:18:21 +0200

    Pages | index: remove AppFrame

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,34 +1,9 @@
-import React, { useState } from "react";
+import React from "react";
 
-import AppFrame from "../components/AppFrame";
-import SectorCard from "../components/SectorCard";
 import Home from "../components/Home";
 
-import data from "../../public/data.json";
-
-const TEST_MENU1 = data.map(item => ({
-  ...item,
-  id: item.title,
-  text: item.title
-}));
-
 function Index() {
-  const [pageContent, setPageContent] = useState(null);
-
-  function handleClickItemMenu(item) {
-    setPageContent(item);
-  }
-
-  return (
-    <AppFrame
-      menuList={[TEST_MENU1]}
-      onClickItemMenu={handleClickItemMenu}
-      title=""
-    >
-      {pageContent && <SectorCard data={pageContent} />}
-      {!pageContent && <Home />}
-    </AppFrame>
-  );
+  return <Home />;
 }
 
 Index.propTypes = {};
