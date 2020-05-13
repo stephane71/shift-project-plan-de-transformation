e8ece925f13a46c878a8fb5bf9dcfcb819f5c17a
stephane71 <stephanemaire71@gmail.com>
2020-05-13 20:20:20 +0200

    NextJS config | add raw-loader

--- next.config.js
+++ next.config.js
@@ -2,5 +2,19 @@ require("dotenv").config({ path: "./.env" });
 
 module.exports = {
   env: {},
-  target: "serverless"
+  target: "serverless",
+  webpack: config => {
+    return {
+      ...config,
+      module: {
+        ...config.module,
+        rules: config.module.rules.concat([
+          {
+            test: /\.md$/,
+            loader: "raw-loader"
+          }
+        ])
+      }
+    };
+  }
 };
