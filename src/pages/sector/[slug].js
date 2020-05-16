316b5887dbe8cafb9940243209f5f283765a9086
stephane71 <stephanemaire71@gmail.com>
2020-05-16 16:58:56 +0200

    Sector | add Container maw width "md"

--- src/pages/sector/[slug].js
+++ src/pages/sector/[slug].js
@@ -1,7 +1,9 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { makeStyles } from "@material-ui/core/styles";
+
 import Button from "@material-ui/core/Button";
+import Container from "@material-ui/core/Container";
 
 import getArticleList from "../../utils/getArticleList";
 import getArticle from "../../utils/getArticle";
@@ -20,18 +22,20 @@ export default function Sector({ content, slug }) {
   const classes = useStyles();
 
   return (
-    <div className={classes.article}>
-      <div className={classes.articleTopBar}>
-        <Button
-          size="small"
-          target="_blank"
-          href={`https://github.com/stephane71/shift-project-plan-de-transformation/blob/master/src/articles/${slug}.md`}
-        >
-          Editer cette page
-        </Button>
+    <Container maxWidth="md">
+      <div className={classes.article}>
+        <div className={classes.articleTopBar}>
+          <Button
+            size="small"
+            target="_blank"
+            href={`https://github.com/stephane71/shift-project-plan-de-transformation/blob/master/src/articles/${slug}.md`}
+          >
+            Editer cette page
+          </Button>
+        </div>
+        <Article content={content} />
       </div>
-      <Article content={content} />
-    </div>
+    </Container>
   );
 }
 
